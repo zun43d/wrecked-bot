10daca849b41d78466e482ef90050e199c4b280b
zun43d <zunaedahmed.info@gmail.com>
2022-10-28 02:23:35 +0600

    Latest version

--- commands/trickOrTreat.js
+++ commands/trickOrTreat.js
@@ -1,7 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js')
 
-const trickOrTreat = () => {
+const cooldown = 1000 * 60 * 60 * 6
+
+let talkedRecently = new Set()
+
+const trickOrTreat = (userid) => {
+	if (talkedRecently.has(userid)) {
+		return "Don't be greedy! ^_^ Come back after some while."
+	}
+
 	const random = (Math.random() * 10).toFixed(2)
+
+	talkedRecently.add(userid)
+	setTimeout(() => {
+		// Removes the user from the set after a minute
+		talkedRecently.delete(userid)
+	}, cooldown)
 
 	return random > 8.5 ? '🍬 Treat!' : '🎃 Trick!'
 }
@@ -11,6 +25,6 @@ module.exports = {
 		.setName('trickortreat')
 		.setDescription('Trick or treat!'),
 	async execute(interaction) {
-		await interaction.reply(trickOrTreat())
+		await interaction.reply(trickOrTreat(interaction.user.id))
 	},
 }
