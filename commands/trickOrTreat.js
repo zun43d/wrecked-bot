072518f611f5ec8266d6f6dfdfd3ca197e531165
zun43d <zunaedahmed.info@gmail.com>
2022-10-27 23:10:54 +0600

    Bot init

---
+++ commands/trickOrTreat.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+const trickOrTreat = () => {
+	const random = (Math.random() * 10).toFixed(2)
+
+	return random > 6.5 ? '🍬 Treat!' : '🎃 Trick!'
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('trickortreat')
+		.setDescription('Trick or treat!'),
+	async execute(interaction) {
+		await interaction.reply(trickOrTreat())
+	},
+}
