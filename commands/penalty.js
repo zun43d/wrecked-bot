21ca60f9734bc1280fa7a937f3dbe3bf225729c4
Zunaed Ahmed <65343827+zun43d@users.noreply.github.com>
2022-11-30 17:05:57 +0000

    bits of fixes

--- commands/penalty.js
+++ commands/penalty.js
@@ -1,65 +1,70 @@
 const fs = require('fs')
 const { SlashCommandBuilder } = require('discord.js')
 
-const scoreBoard = []
+let scoreBoard = []
+
+let globalUID = '';
 
 const msg = {
-	goal: 'Goal!!!!! ⚽',
-	miss: 'Miss! ⛔',
+  goal: 'Goal!!!!! ⚽',
+  miss: 'Miss! ⛔',
 }
 
 const cooldown = 1000 /*ms*/ * 60 /*sec*/ * 30 /*min*/ /* * */ /*4*/ /*hrs*/
-const missChance = 65
+const missChance = 50
+// const missChance = (globalUID == '433503494322913290') ? 90 : 50
 
 let triedRecently = new Set()
 
 const setTriedRecently = (userid) => {
-	triedRecently.add(userid)
-	setTimeout(() => {
-		// Removes the user from the set after cooldown
-		triedRecently.delete(userid)
-	}, cooldown)
+  triedRecently.add(userid)
+  setTimeout(() => {
+    // Removes the user from the set after cooldown
+    triedRecently.delete(userid)
+  }, cooldown)
 }
 
 const goalOrMiss = () => {
-	const random = (Math.random() * 100).toFixed(2)
-	return random > missChance ? true : false
+  const random = (Math.random() * 100).toFixed(2)
+  return random > missChance ? true : false
 }
 
 // sort scoreBoard in descending order
 const sortScoreBoard = () => {
-	scoreBoard.sort((a, b) => b.score - a.score)
+  scoreBoard.sort((a, b) => b.score - a.score)
 }
 
 // save the scoreboard to a JSON file
 const saveScoreBoard = () => {
-	const data = JSON.stringify(scoreBoard, null, 2)
-	fs.writeFile('score/scoreBoard.json', data, (err) => {
-		if (err) {
-			throw err
-		}
-		console.log('Score Board saved to a file - ' + Date(Date.now()).toString())
-	})
+  const data = JSON.stringify(scoreBoard, null, 2)
+  fs.writeFile('score/scoreBoard.json', data, (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Score Board saved to a file - ' + Date(Date.now()).toString())
+  })
+  scoreBoard = []
 }
 
 const handleUser = (userid) => {
-	// check if scoreboard.json exits, if yes read it then parse it to scoreBoard
-	if (fs.existsSync('score/scoreBoard.json')) {
-		const data = fs.readFileSync('score/scoreBoard.json')
-		const dataObj = JSON.parse(data)
-		scoreBoard.push(...dataObj)
-	}
+  // check if scoreboard.json exits, if yes read it then parse it to scoreBoard
+  if (fs.existsSync('score/scoreBoard.json')) {
+    const data = fs.readFileSync('score/scoreBoard.json')
+    const dataObj = JSON.parse(data)
+    scoreBoard = []
+    scoreBoard.push(...dataObj)
+  }
 
-	const user = scoreBoard.find((user) => user.id === userid)
-	if (user) {
-		user.score++
-	} else {
-		scoreBoard.push({ id: userid, score: 1 })
-	}
+  const user = scoreBoard.find((user) => user.id === userid)
+  if (user) {
+    user.score++
+  } else {
+    scoreBoard.push({ id: userid, score: 1 })
+  }
 
-	// sort and save the scoreboard
-	sortScoreBoard()
-	saveScoreBoard()
+  // sort and save the scoreboard
+  sortScoreBoard()
+  saveScoreBoard()
 }
 
 // setTimeout(() => {
@@ -68,27 +73,28 @@ const handleUser = (userid) => {
 // }, 1000 /*ms*/ * 60 /*sec*/ * 1 /*min*/)
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('penalty')
-		.setDescription('Shoot to the goal post!'),
-	async execute(interaction) {
-		// await interaction.reply('Shooting!')
-		const userId = interaction.user.id
+  data: new SlashCommandBuilder()
+    .setName('penalty')
+    .setDescription('Shoot to the goal post!'),
+  async execute(interaction) {
+    // await interaction.reply('Shooting!')
+    const userId = interaction.user.id
 
-		// Check if user has already tried
-		if (triedRecently.has(userId)) {
-			await interaction.reply('Please wait and let others try! ^_^')
-			return
-		}
+    // Check if user has already tried
+    if (triedRecently.has(userId)) {
+      await interaction.reply('Your leg is in pain. You should rest for a while! ^0^')
+      return
+    }
 
-		setTriedRecently(userId)
+    setTriedRecently(userId)
 
-		// Do goalOrMiss and handleUser
-		if (goalOrMiss()) {
-			await interaction.reply(msg.goal)
-			handleUser(userId)
-		} else {
-			await interaction.reply(msg.miss)
-		}
-	},
+    globalUID = userId
+    // Do goalOrMiss and handleUser
+    if (goalOrMiss()) {
+      await interaction.reply(msg.goal)
+      handleUser(userId)
+    } else {
+      await interaction.reply(msg.miss)
+    }
+  },
 }
--- commands/scoreboard.js
+++ commands/scoreboard.js
@@ -3,114 +3,113 @@ const schedule = require('node-schedule')
 const { SlashCommandBuilder, WebhookClient } = require('discord.js')
 
 const webhookClient = new WebhookClient({
-	id: process.env.webhookId,
-	token: process.env.webhookToken,
+  id: process.env.webhookId,
+  token: process.env.webhookToken,
 })
 
 function addHours(numOfHours, date = new Date()) {
-	date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000)
+  date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000)
 
-	return date
+  return date
 }
 
 // get initTime from data.json
 const getInitTime = () => {
-	const data = fs.readFileSync('data.json')
-	const dataObj = JSON.parse(data)
+  const data = fs.readFileSync('data.json')
+  const dataObj = JSON.parse(data)
 
-	return dataObj.initTime
+  return dataObj.initTime
 }
 
 // calculate the ending time
 const getEndTime = () => {
-	const initTime = getInitTime()
-	const endTime = addHours(24, new Date(initTime))
+  const initTime = getInitTime()
+  const endTime = addHours(24, new Date(initTime))
 
-	// convert endTime to UTC
-	const endTimeUTC = new Date(endTime.toUTCString()).toLocaleString()
+  // convert endTime to UTC
+  const endTimeUTC = new Date(endTime.toUTCString()).toLocaleString()
 
-	return endTimeUTC
+  return endTimeUTC
 }
 
 // reset scoreBoard.json by deleting it and update initTime in data.json
 const resetScoreBoard = () => {
-	// delete scoreBoard.json
-	fs.unlinkSync('score/scoreBoard.json')
+  // delete scoreBoard.json
+  fs.unlinkSync('score/scoreBoard.json')
 
-	// update initTime in data.json
-	const data = fs.readFileSync('data.json')
-	const dataObj = JSON.parse(data)
-	dataObj.initTime = new Date().toLocaleString()
+  // update initTime in data.json
+  const data = fs.readFileSync('data.json')
+  const dataObj = JSON.parse(data)
+  dataObj.initTime = new Date().toLocaleString()
 
-	const dataJSON = JSON.stringify(dataObj, null, 2)
-	fs.writeFileSync('data.json', dataJSON)
+  const dataJSON = JSON.stringify(dataObj, null, 2)
+  fs.writeFileSync('data.json', dataJSON)
 }
 
-schedule.scheduleJob(new Date(getEndTime()), () => {
-	webhookClient.send({
-		content: 'Scoreboard that ended at ' + getEndTime(),
-		embeds: [embed()],
-	})
-	resetScoreBoard()
+schedule.scheduleJob(new Date(getEndTime()), async () => {
+  await webhookClient.send({
+    content: 'Scoreboard that ended at ' + getEndTime(),
+    embeds: [embed()],
+  })
+  resetScoreBoard()
 })
 
 // read scoreBoard.json and get the top 10 scores
 const scoreData = () => {
-	if (fs.existsSync('score/scoreBoard.json')) {
-		return fs.readFileSync('score/scoreBoard.json')
-	}
+  if (fs.existsSync('score/scoreBoard.json')) {
+    return fs.readFileSync('score/scoreBoard.json')
+  }
 
-	return '[]'
+  return '[]'
 }
 const getTop10 = () => {
-	const scoreBoard = JSON.parse(scoreData())
+  const scoreBoard = JSON.parse(scoreData())
 
-	// sort scoreBoard in descending order
-	// scoreBoard.sort((a, b) => b.score - a.score)
+  // sort scoreBoard in descending order
+  // scoreBoard.sort((a, b) => b.score - a.score)
 
-	// get top 10 scores
-	const top10 = scoreBoard.slice(0, 10)
+  // get top 10 scores
+  const top10 = scoreBoard.slice(0, 10)
 
-	return top10
+  return top10
 }
 
 const top10List = () =>
-	getTop10()
-		.map((obj, i) => {
-			return `\`${i + 1}. \` <@${obj.id}> **·** Score: **${obj.score}** ${
-				i === 0 ? '🥇' : ''
-			}`
-		})
-		.join('\n')
+  getTop10()
+    .map((obj, i) => {
+      return `\`${i + 1}. \` <@${obj.id}> **·** Score: **${obj.score}** ${i === 0 ? '🥇' : ''
+        }`
+    })
+    .join('\n')
 
 const embed = () => {
-	return {
-		color: 0xe97532,
-		title: 'Scoreboard',
-		description: `Remember: Scoreboard resets every 24 hours.\n\n`,
-		fields: [
-			{
-				name: 'Top 10 people with the most scores.',
-				value: top10List() || '*No one has scored yet.*\n',
-				inline: false,
-			},
-		],
-		footer: {
-			text: `Scoreboard will reset at ${getEndTime()} UTC \n\n`,
-		},
-		// timestamp: new Date().toISOString(),
-	}
+  return {
+    color: 0xe97532,
+    title: 'Scoreboard',
+    description: `Remember: Scoreboard resets every 24 hours.\n\n`,
+    fields: [
+      {
+        name: 'Top 10 people with the most scores.',
+        value: top10List() || '*No one has scored yet.*\n',
+        inline: false,
+      },
+    ],
+    footer: {
+      text: `Scoreboard will reset at ${getEndTime()} UTC \n\n`,
+    },
+    // timestamp: new Date().toISOString(),
+  }
 }
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('scoreboard')
-		.setDescription('Top 10 players with the most score!'),
-	async execute(interaction) {
-		// const userId = interaction.user.id
-		await interaction.reply({
-			embeds: [embed()],
-			// ephemeral: true,
-		})
-	},
+  data: new SlashCommandBuilder()
+    .setName('scoreboard')
+    .setDescription('Top 10 players with the most score!'),
+  async execute(interaction) {
+    // const userId = interaction.user.id
+    await interaction.reply({
+      embeds: [embed()],
+      // ephemeral: true,
+    })
+  },
 }
